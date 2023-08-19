e2c6c145a655add652452197c4bc65628ce90030
Your Name <you@example.com>
2023-08-19 09:55:35 +0530

    New proxy code added

---
+++ src/main/java/design/proxy/Account.java
@@ -0,0 +1,6 @@
+package design.proxy;
+
+public interface Account {
+    public void withdraw();
+    void getAccountNumber();
+}
---
+++ src/main/java/design/proxy/BankAccount.java
@@ -0,0 +1,13 @@
+package design.proxy;
+
+public class BankAccount implements  Account{
+    @Override
+    public void withdraw() {
+
+    }
+
+    @Override
+    public void getAccountNumber() {
+
+    }
+}
